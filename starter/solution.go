16ef7162d58cdc4add1fdb97ce4a25ec4324b857
Perfect Makanju <damiperfect@gmail.com>
2022-12-04 07:14:12 +0000

    chore: create starter module for easy copy pasting

---
+++ starter/solution.go
@@ -0,0 +1,58 @@
+package day4
+
+import (
+	"bufio"
+	"os"
+)
+
+func Solve() (uint64, error) {
+	return Solve1()
+}
+
+func Solve1() (uint64, error) {
+	//inputFile, err := os.Open("day4/sample.txt")
+	inputFile, err := os.Open("day4/input.txt")
+	defer inputFile.Close()
+	if err != nil {
+		return 0, err
+	}
+
+	inputReader := bufio.NewScanner(inputFile)
+	if err != nil {
+		return 0, err
+	}
+
+	totalScore := uint64(0)
+	for inputReader.Scan() {
+	}
+
+	if inputReader.Err() != nil {
+		return 0, inputReader.Err()
+	}
+
+	return totalScore, nil
+}
+
+func Solve2() (uint64, error) {
+	//inputFile, err := os.Open("day4/sample.txt")
+	inputFile, err := os.Open("day4/input.txt")
+	defer inputFile.Close()
+	if err != nil {
+		return 0, err
+	}
+
+	inputReader := bufio.NewScanner(inputFile)
+	if err != nil {
+		return 0, err
+	}
+
+	totalScore := uint64(0)
+	for inputReader.Scan() {
+	}
+
+	if inputReader.Err() != nil {
+		return 0, inputReader.Err()
+	}
+
+	return totalScore, nil
+}
